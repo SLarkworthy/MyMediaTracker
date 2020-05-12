e02fa47a1508fedf50cb6fa8f0ffc27bb664b3a7
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-05-12 13:58:37 -0400

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     has_many :media, through: :experiences
 
     has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 end
