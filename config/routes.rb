b0906c7bfb0d5504220e83ff168ec97987fb48ae
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-05-19 14:21:06 -0400

    trimmed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,15 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
   delete '/logout' => 'sessions#destroy'
-  
+
   get '/users/:id/favorites' => 'users#favorites'
 
   get '/auth/:provider/callback' => 'sessions#create'
 
-  resources :categories
-  resources :media do 
-    resources :experiences, only: [:new, :index, :edit]
+  resources :media, only: [:index, :new, :create] do 
+    resources :experiences, only: [:index, :new, :edit]
   end
-  resources :experiences
-  resources :users
+  resources :experiences, only: [:show, :update, :destroy, :index, :create]
+  resources :users, only: [:show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
