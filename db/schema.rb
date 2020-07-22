162c6895df072b8bb017d3f6466218f2fd3a9a86
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-07-22 16:23:05 -0400

    changed database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_11_155909) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
